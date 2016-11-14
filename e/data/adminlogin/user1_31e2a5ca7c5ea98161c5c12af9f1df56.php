a10279aa5e083b5bbd7e806690efa5a7fb58bd28
561261067 <561261067@qq.com>
2016-11-14 14:21:25 +0800

    添加了帝国CMS

---
+++ e/data/adminlogin/user1_31e2a5ca7c5ea98161c5c12af9f1df56.php
@@ -0,0 +1,12 @@
+<?php
+$ecms_adminloginr=array();
+$ecms_adminloginr=Array('userid'=>'1',
+'ernd'=>'JzywKCF6FSf90AVo4QBkCWp1N8A',
+'erndtwo'=>'lcLaM9DLC2ZWAXqIbxH3',
+'erndadd'=>'KpaSUwn1OvazO1qFhjnCgMUbtENIvpza',
+'ehash'=>'RI8XSW7qIcrzU6nkFiG4',
+'ehashname'=>'ehash_5oOg',
+'rhash'=>'dOw0zjjcwcm9',
+'rhashname'=>'rhash_HMn6',
+'edbdata'=>'1');
+?>
